208e1f59832840f251d3c6ebe7ee91046b5eaf84
Eddú Meléndez <eddu.melendez@gmail.com>
2020-11-15 22:49:16 -0600

    Add arch

--- src/sdkman_java_migrations/util/sdkman.clj
+++ src/sdkman_java_migrations/util/sdkman.clj
@@ -2,9 +2,13 @@
 
 (def ^:private platforms
   {:linux   {:x64 "LINUX_64"
-             :aarch64 "LINUX_ARM64"}
-   :mac     {:x64 "MAC_OSX"}
-   :windows {:x64 "WINDOWS_64"}})
+             :x86 "LINUX_64"
+             :aarch64 "LINUX_ARM64"
+             :arm "LINUX_ARM64"}
+   :mac     {:x64 "MAC_OSX"
+             :x86 "MAC_OSX"}
+   :windows {:x64 "WINDOWS_64"
+             :x86 "WINDOWS_64"}})
 
 (defn platform
   [os arch]
