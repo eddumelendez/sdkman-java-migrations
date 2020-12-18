b15f71c65ec0049347a1928145bc08c845cd8f4f
Eddú Meléndez <eddu.melendez@gmail.com>
2020-12-18 00:22:49 -0600

    Upgrade clj-http version to 3.11.0

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[clj-http "3.10.3"]
+  :dependencies [[clj-http "3.11.0"]
                  [org.clojure/clojure "1.10.1"]
                  [org.clojure/data.json "0.2.6"]
                  [org.clojure/data.xml "0.0.8"]
